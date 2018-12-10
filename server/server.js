0f7f1245a9f32432113a42d7029f737ca1fc9449
Sarvesh Singh <sarveshs981@gmai.com>
2018-12-10 21:57:47 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -22,12 +22,11 @@ app.post('/todos', (req,res) => {
     res.status(400).send(e);
   });
 });
-
 app.get('/todos', (req,res) => {
   Todo.find().then((todos) => {
      res.send({todos});
   }, (e) => {
-    res.status(400).send(e);
+  return  res.status(400).send(e);
   });
 });
 
@@ -48,8 +47,26 @@ Todo.findById(id).then((todo) => {
 }).catch((e) => {
   res.status(400).send();
 });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+Todo.findByIdAndRemove(id).then((todo) => {
+  if (!todo) {
+    return res.status(404).send();
+  }
+
+  res.send(todo);
+}).catch((e) => {
+  res.status(400).send();
+});
 
 });
+
 app.listen(port,() => {
   console.log(`Started up at port ${port}`);
 });
